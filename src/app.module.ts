0ca4809e51b441c8f33d2f57d34b40c661604a22
komar.izmi <komar.izmi@gmail.com>
2022-11-19 15:14:51 +0700

    delete files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { RolesModule } from './roles/roles.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthController } from './nest/auth/auth.controller';
 import { AuthLogModule } from './auth-log/auth-log.module';
+import { AuthModule } from './auth/auth.module';
 import { HasRolesModule } from './has-roles/has-roles.module';
+import { Module } from '@nestjs/common';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, RolesModule, AuthModule, AuthLogModule, HasRolesModule],
-  controllers: [AppController, AuthController],
+  imports: [
+    UsersModule,
+    RolesModule,
+    AuthModule,
+    AuthLogModule,
+    HasRolesModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
